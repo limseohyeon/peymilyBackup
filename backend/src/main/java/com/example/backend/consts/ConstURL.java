73dc4a02d396ba6c58907e248fb322e4e272a33a
SangYoun <lsyun1234@naver.com>
2023-05-07 22:56:02 +0900

    schedule

--- backend/src/main/java/com/example/backend/consts/ConstURL.java
+++ backend/src/main/java/com/example/backend/consts/ConstURL.java
@@ -3,5 +3,5 @@ package com.example.backend.consts;
 import org.hibernate.validator.constraints.URL;
 
 public class ConstURL {
-    public static final String url = "ec2-13-125-244-38.ap-northeast-2.compute.amazonaws.com";
+    public static final String url = "ec2-43-200-8-47.ap-northeast-2.compute.amazonaws.com";
 }
