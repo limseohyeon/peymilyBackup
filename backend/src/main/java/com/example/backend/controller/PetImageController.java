736b07bfe48adf17acdd7c261cfa60b2861fbde4
SangYoun <lsyun1234@naver.com>
2023-05-24 13:36:21 +0900

    이미지 나오게 수정

--- backend/src/main/java/com/example/backend/controller/PetImageController.java
+++ backend/src/main/java/com/example/backend/controller/PetImageController.java
@@ -1,6 +1,9 @@
 package com.example.backend.controller;
 
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
 import com.example.backend.util.FileUploadUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
@@ -14,19 +17,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/pet/{inviter}")
+@RequestMapping("/pet/{email}")
 public class PetImageController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping("/uploadImage/{petId}")
-    public ResponseEntity<String> uploadImage(@PathVariable("inviter") String inviter,
+    public ResponseEntity<String> uploadImage(@PathVariable("email") String email,
                                               @PathVariable("petId") Long petId,
                                               @RequestParam("file") MultipartFile file) throws IOException {
         // 파일 이름에서 확장자 추출
         String originalFilename = StringUtils.cleanPath(file.getOriginalFilename());
         String fileName = petId.toString() + ".jpg"; // petId를 기준으로 파일 이름을 정함
         String uploadDir = "image-uploads/";
+
+        Optional<User> user = userRepository.findByEmail(email);
+        String inviter = user.get().getInviter();
+
         // 유저 정보를 기반으로 업로드 디렉토리 생성
         String userUploadDir = uploadDir + inviter + "/";
         FileUploadUtil.saveFile(userUploadDir, fileName, file);
@@ -35,9 +46,13 @@ public class PetImageController {
     }
 
     @GetMapping("/downloadImage/{imageName:.+}") // 이미지 확장자를 포함하기 위해 ".+"를 추가
-    public ResponseEntity<Resource> downloadImage(@PathVariable("inviter") String inviter,
+    public ResponseEntity<Resource> downloadImage(@PathVariable("email") String email,
                                                   @PathVariable String imageName) throws IOException {
         String downloadDir = "image-uploads/";
+
+        Optional<User> user = userRepository.findByEmail(email);
+        String inviter = user.get().getInviter();
+
         // 유저 정보를 기반으로 다운로드 디렉토리 지정
         String userDownloadDir = downloadDir + "/" + inviter + "/";
         Path path = Paths.get(userDownloadDir, imageName);
@@ -59,9 +74,12 @@ public class PetImageController {
     }
 
     @PutMapping("/updateImage/{petId}")
-    public ResponseEntity<String> updateImage(@PathVariable("inviter") String inviter,
+    public ResponseEntity<String> updateImage(@PathVariable("email") String email,
                                               @PathVariable("petId") Long petId,
                                               @RequestParam("file") MultipartFile file) throws IOException {
+        Optional<User> user = userRepository.findByEmail(email);
+        String inviter = user.get().getInviter();
+
         String originalFilename = StringUtils.cleanPath(file.getOriginalFilename());
         String fileName = petId.toString() + StringUtils.getFilenameExtension(originalFilename);
         String uploadDir = "image-uploads/";
@@ -72,10 +90,14 @@ public class PetImageController {
     }
 
     @DeleteMapping("/deleteImage/{petId}")
-    public ResponseEntity<String> deleteImage(@PathVariable("inviter") String inviter,
+    public ResponseEntity<String> deleteImage(@PathVariable("email") String email,
                                               @PathVariable("petId") String petId) throws IOException {
         String fileName = petId;
         String uploadDir = "image-uploads/";
+
+        Optional<User> user = userRepository.findByEmail(email);
+        String inviter = user.get().getInviter();
+
         String userUploadDir = uploadDir + inviter + "/";
         Path filePath = Paths.get(userUploadDir, fileName);
 
