d88156847ffa884bc323f3e67cdc566447dbe0a9
SangYoun <lsyun1234@naver.com>
2023-09-23 21:23:48 +0900

    커뮤니티, 초대 CRUD 코드 작성

--- backend/src/main/java/com/example/backend/repository/CommunityRepository.java
+++ backend/src/main/java/com/example/backend/repository/CommunityRepository.java
@@ -16,6 +16,6 @@ public interface CommunityRepository extends JpaRepository<Community, Long> {
     // @Query 어노테이션으로 DELETE 또는 UPDATE 쿼리를 실행할 때는 @Modifying 어노테이션도 함께 사용해야 합니다.
     // 이 어노테이션은 해당 쿼리가 데이터베이스를 수정하는 것임을 알려줍니다.
     @Modifying
-    @Query("DELETE c FROM Community c WHERE c.communityId = ?1")
+    @Query("DELETE FROM Community c WHERE c.communityId = ?1")
     int deleteByCommunityId(Long communityId);
 }
