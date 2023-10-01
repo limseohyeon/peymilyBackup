15f1761c22e47ba88a89646b903987316d831459
seohyeon <iacsbe36@gmail.com>
2023-10-01 19:58:02 +0900

    부양육자 수정

--- backend/src/main/java/com/example/backend/controller/PetLinkController.java
+++ backend/src/main/java/com/example/backend/controller/PetLinkController.java
@@ -147,7 +147,7 @@ public class PetLinkController {
 //    @GetMapping("/rearer")
 
 //부양육자 읽기
-    @GetMapping("/sub-rearer/{petId}/{owner}/{inviter}")
+    @GetMapping("/sub-rearer/{petId}/{inviter}")
     public ResponseEntity<List<PetLink>> ReadSubRearer(@PathVariable("petId") Long petId,
                                                        @PathVariable("owner") String owner,
                                                        @PathVariable("inviter") String inviter
