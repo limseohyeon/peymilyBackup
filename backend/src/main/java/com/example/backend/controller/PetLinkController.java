f32f7eb5f7885959698fa15528d353a361508918
seohyeon <iacsbe36@gmail.com>
2023-10-02 17:18:11 +0900

    양육자 수정

--- backend/src/main/java/com/example/backend/controller/PetLinkController.java
+++ backend/src/main/java/com/example/backend/controller/PetLinkController.java
@@ -65,7 +65,8 @@ public class PetLinkController {
         return ResponseEntity.notFound().build();
     }
 
-//owner 를 이용해서 특정 petLink 찾아오기
+//사용자가 포함된 petLink 중 특정 petLink 찾아오기
+
     @GetMapping("/get/{owner}/{petId}")
     public ResponseEntity <PetLink> ReadLinkedPet(@PathVariable("owner") String owner,
                                                   @PathVariable("petId") Long petId){
@@ -78,7 +79,7 @@ public class PetLinkController {
         return ResponseEntity.notFound().build();
     }
 
-//owner 를 이용해서 모든 petLink 찾아오기
+//사용자가 포함된 이용해서 모든 petLink 찾아오기
     @GetMapping("/getAll/{owner}")
     public ResponseEntity<List<PetLink>> ReadAllLinkedPet(@PathVariable ("owner") String owner) {
 
