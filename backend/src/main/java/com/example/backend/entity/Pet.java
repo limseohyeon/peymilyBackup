e6592c24a0f73c72d27633e4e203240ea8a1e41c
SangYoun <lsyun1234@naver.com>
2023-10-15 21:54:25 +0900

    스케줄 수정

--- backend/src/main/java/com/example/backend/entity/Pet.java
+++ backend/src/main/java/com/example/backend/entity/Pet.java
@@ -27,7 +27,7 @@ public class Pet implements Serializable {
     // Serializable은 객체를 전송또는 저장할 때 필요하다
     // 즉, 전송 또는 저장을 하지 않는 단순 출력문만 있는 경우에는 불필요하다
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName="userId") // 230709 긴급 수정
