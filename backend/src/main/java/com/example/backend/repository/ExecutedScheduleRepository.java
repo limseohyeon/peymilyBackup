68533f7a5b72956cbc2e03e1a045ceea1cfac1e6
SangYoun <lsyun1234@naver.com>
2023-10-17 23:02:16 +0900

    executed 관련 코드 오류 수정

--- backend/src/main/java/com/example/backend/repository/ExecutedScheduleRepository.java
+++ backend/src/main/java/com/example/backend/repository/ExecutedScheduleRepository.java
@@ -13,7 +13,6 @@ public interface ExecutedScheduleRepository  extends JpaRepository<ExecutedSched
     List<ExecutedSchedule> findByScheduleId(Long scheduleId);
     void deleteByExecutedId(Long executedId);
 
-    @Modifying
     @Query("SELECT es FROM ExecutedSchedule es WHERE es.scheduleId = :scheduleId AND es.date = :date")
     ExecutedSchedule findExecutedScheduleByScheduleIdAndDate(@Param("scheduleId") Long scheduleId,
                                                              @Param("date") String date);
