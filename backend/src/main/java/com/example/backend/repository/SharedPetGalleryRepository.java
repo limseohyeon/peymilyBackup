d0e981af1bcba8e916bc7f15a22a061a58adc1b1
seohyeon <iacsbe36@gmail.com>
2023-10-17 17:10:01 +0900

    공유 사진첩 수정

--- backend/src/main/java/com/example/backend/repository/SharedPetGalleryRepository.java
+++ backend/src/main/java/com/example/backend/repository/SharedPetGalleryRepository.java
@@ -5,19 +5,47 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface SharedPetGalleryRepository extends JpaRepository<SharedPetGallery, Long> {
 
+    //특정 사진 가져오기
     @Query("SELECT c FROM SharedPetGallery c WHERE c.photoId = ?1")
     SharedPetGallery findPostById(Long photoId);
 
-    @Query("SELECT c FROM SharedPetGallery c WHERE c.email = ?1")
-    SharedPetGallery findPostByEmail(String email);
+    //해당 펫에 속한 모든 사진 가져오기
+    @Query("SELECT c FROM SharedPetGallery c WHERE c.petId = ?1")
+   List<SharedPetGallery> findPostByPetId(Long petId);
 
+    //해당 사용자에 속한 모든 사진 가져오기
+    @Query("SELECT c FROM SharedPetGallery c WHERE c.email = ?1")
+    List<SharedPetGallery> findPostByEmail(String email);
+
+    // 사용자와 펫에 속한 모든 사진 가져오기
+    @Query("SELECT c FROM SharedPetGallery c WHERE c.email = ?1 AND c.petId = ?2")
+    List<SharedPetGallery> findPostsByUserAndPetId(String email, Long petId);
+
+
+
+    //특정 사진 지우기
     @Modifying
     @Query("DELETE FROM SharedPetGallery c WHERE c.photoId = ?1")
     int deleteByPhotoId(Long photoId);
 
+    //사용자에 속한 모든 사진 지우기(사용자 계정 탈퇴)
     @Modifying
     @Query("DELETE FROM SharedPetGallery c WHERE c.email = ?1")
     int deleteByEmail(String email);
+
+    //사용자와 펫에 속한 모든 사진 지우기(양육자 삭제)
+    @Modifying
+    @Query("DELETE FROM SharedPetGallery c WHERE c.email = ?1 AND c.petId = ?2")
+    int deletePostsByUserAndPetId(String email, Long petId);
+
+    //해당 펫에 속한 모든 사진 지우기(펫 계정 지우기)
+    @Modifying
+    @Query("DELETE FROM SharedPetGallery c WHERE c.petId = ?1")
+    int deleteByPetId(Long petId);
+
+
 }
--- backend/src/main/java/com/example/backend/service/SharedPetGalleryService.java
+++ backend/src/main/java/com/example/backend/service/SharedPetGalleryService.java
@@ -15,25 +15,32 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class SharedPetGalleryService {
-
-
     @Autowired
     private final SharedPetGalleryRepository sharedPetGalleryRepository;
     @Autowired
     private final UserService userService;
+    @Autowired
+    private final PetService petService;
 
     //게시글 Id로 불러오기
     public SharedPetGallery findPostById(Long photoId) {
         return sharedPetGalleryRepository.findPostById(photoId);
     }
     //사용자 Email 로 불러오기
-    public SharedPetGallery findPostByEmail(String email) {
+    public List<SharedPetGallery> findAllPostByEmail(String email) {
         return sharedPetGalleryRepository.findPostByEmail(email);
     }
-    //모든 게시글 불러오기
-    public List<SharedPetGallery> findAllPost() { return sharedPetGalleryRepository.findAll(); }
-//게시글 저장하기
-    public SharedPetGallery savePhoto(SharedPetGalleryRequest sharedPetGalleryRequest, String email) {
+    //petId로 불러오기
+    public List<SharedPetGallery> findAllPostByPetId(Long petId) {
+        return sharedPetGalleryRepository.findPostByPetId(petId);
+    }
+    //petId && Email 로 불러오기
+    public List<SharedPetGallery> findAllPostByUserAndPetId( String email,Long petId) {
+        return sharedPetGalleryRepository.findPostsByUserAndPetId(email,petId);
+    }
+
+    //게시글 저장하기
+    public SharedPetGallery savePhoto(SharedPetGalleryRequest sharedPetGalleryRequest, String email, Long petId) {
         Optional<User> optionalUser = userService.findByEmail(email);
 
         if (optionalUser.isPresent()) {
@@ -43,6 +50,7 @@ public class SharedPetGalleryService {
                     .title(sharedPetGalleryRequest.getTitle())
                     .wrote(sharedPetGalleryRequest.getWrote())
                     .date(sharedPetGalleryRequest.getDate())
+                    .petId(sharedPetGalleryRequest.getPetId())
                     .build();
 
             return sharedPetGalleryRepository.save(sharedPetGallery);
@@ -60,12 +68,28 @@ public class SharedPetGalleryService {
 
         return sharedPetGalleryRepository.save(sharedPetGallery);
     }
-//게시글 삭제
+//특정 게시글 삭제
     public SharedPetGallery deleteSharedPetGallery(Long photoId) {
         SharedPetGallery sharedPetGallery = findPostById(photoId);
-
         sharedPetGalleryRepository.deleteByPhotoId(photoId);
-
         return sharedPetGallery;
+    }
+    //사용자에 속한 모든 게시글 삭제 - (계정 탈퇴)
+    public List<SharedPetGallery> deleteSharedPetGalleryByEmail(String email) {
+        List<SharedPetGallery> allSharedPetGallery = findAllPostByEmail(email);
+        sharedPetGalleryRepository.deleteByEmail(email);
+        return allSharedPetGallery;
+    }
+    //펫에 속한 모든 게시글 삭제 - (펫 계정 지우기)
+    public List<SharedPetGallery> deleteSharedPetGalleryByPetId(Long petId) {
+        List<SharedPetGallery> allSharedPetGallery = findAllPostByPetId(petId);
+        sharedPetGalleryRepository.deleteByPetId(petId);
+        return allSharedPetGallery;
+    }
+    //펫&&사용자에 속한 게시글 삭제 - (양육자 삭제)
+    public List<SharedPetGallery> deleteSharedPetGalleryByPetIdAndEmail( String email, Long petId) {
+        List<SharedPetGallery> allSharedPetGallery = findAllPostByUserAndPetId(email, petId);
+        sharedPetGalleryRepository.deletePostsByUserAndPetId(email,petId);
+        return allSharedPetGallery;
     }
 }
